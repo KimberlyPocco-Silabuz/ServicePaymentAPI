8bb20656d8a29fe4ef80d7371c518c0cd770346c
KimberlyPocco <kimberly.poccop@gmail.com>
2022-12-17 15:10:22 -0500

    creacion de modelos servicespayment

--- ProjectAPI/servicespayment/models.py
+++ ProjectAPI/servicespayment/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+'''
+modelo servicios trabaja con el user por default de django
+'''
+class Services(models.Model):
+    name = models.CharField(max_length=100) 
+    Description = models.TextField()
+    Logo = models.CharField(max_length=100)
+    
+    class Meta:
+        db_table = "services"
+
+'''
+modelo payment
+'''
+class Payment_user(models.Model):
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    service_id = models.ForeignKey(Services,on_delete=models.CASCADE)
+    amount = models.CharField(max_length=100)
+    paymentDate = models.DateTimeField()
+    expirationDate = models.DateTimeField()
+    
+    class Meta:
+        db_table = "payment_user"
+
+'''
+modelo expired  payments
+'''
+class Expired_payments(models.Model):
+    payment_user_id = models.ForeignKey(Payment_user,on_delete=models.CASCADE)
+    penalty_fee_amount= models.IntegerField()
